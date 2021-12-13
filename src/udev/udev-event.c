2990807c94bbc61186c3b7129cda2d86e9ae1d5c
Yu Watanabe <watanabe.yu+github@gmail.com>
2021-12-14 08:21:58 +0900

    udev: warn about truncation of program result
    Closes #21078.
    
    Conflicts:
    	src/udev/udev-event.c

--- src/udev/udev-event.c
+++ src/udev/udev-event.c
@@ -562,6 +562,7 @@ int udev_check_format(const char *value, size_t *offset, const char **hint) {
 static int on_spawn_io(sd_event_source *s, int fd, uint32_t revents, void *userdata) {
         Spawn *spawn = userdata;
         char buf[4096], *p;
+        bool full = false;
         size_t size;
         ssize_t l;
         int r;
@@ -578,7 +579,7 @@ static int on_spawn_io(sd_event_source *s, int fd, uint32_t revents, void *userd
                 size = sizeof(buf);
         }
 
-        l = read(fd, p, size - 1);
+        l = read(fd, p, size - (p == buf));
         if (l < 0) {
                 if (errno == EAGAIN)
                         goto reenable;
@@ -587,6 +588,13 @@ static int on_spawn_io(sd_event_source *s, int fd, uint32_t revents, void *userd
                                        "Failed to read stdout of '%s': %m", spawn->cmd);
 
                 return 0;
+        }
+
+        if ((size_t) l == size) {
+                log_device_warning(spawn->device, "Truncating stdout of '%s' up to %zu byte.",
+                                   spawn->cmd, spawn->result_size);
+                l--;
+                full = true;
         }
 
         p[l] = '\0';
@@ -608,7 +616,7 @@ static int on_spawn_io(sd_event_source *s, int fd, uint32_t revents, void *userd
         }
 
 
-        if (l == 0)
+        if (l == 0 || full)
                 return 0;
 
         /* Re-enable the event source if we did not encounter EOF */
